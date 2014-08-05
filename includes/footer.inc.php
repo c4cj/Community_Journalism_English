4e254f19478dd9056e4a6ded77db18ea6dd685b3
Peter <wilcoxp@cardiff.ac.uk>
2014-08-05 10:00:06 +0100

    Brought English language theme up to date with Werlsh version

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -2,8 +2,8 @@
 		<div class="wrap">
 			<p><?php $url = "http://www.storini.com"; $link = sprintf( __( "Powered by WordPress.", 'storini'), esc_url( $url ) ); echo $link; ?></p>
 			<ul class="hide">
-				<li><a href="<?php echo  get_site_url(); ?>/amdanom-ni-2/"><?php _e("About", 'storini'); ?></a></li>
-				<li><a href="<?php echo  get_site_url(); ?>/cysylltu/"><?php _e("Contact", 'storini'); ?></a></li>
+				<li><a href="<?php echo  get_site_url(); ?>/about/"><?php _e("About", 'storini'); ?></a></li>
+				<li><a href="<?php echo  get_site_url(); ?>/contact/"><?php _e("Contact", 'storini'); ?></a></li>
 				<?php if( get_field('advertising', 'option') ) { ?><li><a href="<?php echo  get_site_url(); ?>/advertising/"><?php _e("Advertising", 'storini'); ?></a></li><?php } else { ?><?php } ?>
 				<?php if( get_field('integrate_storini', 'option') ) { ?>
 					<li><a href="<?php echo  get_site_url(); ?>/become-a-reporter/"><?php _e("Become a local reporter", 'storini'); ?></a></li>
