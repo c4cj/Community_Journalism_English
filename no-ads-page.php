0ba44e58e5ae8030ca70d516b049b1e12ab29fc1
Peter <wilcoxp@cardiff.ac.uk>
2014-08-08 11:46:50 +0100

    Updated dsktop.css for no ads template, removed 'via @Storini' attribution from 'Tweet this' buttons

--- no-ads-page.php
+++ no-ads-page.php
@@ -1,3 +1,7 @@
+<?php
+	/* Template Name: No Ads
+	*/
+?>
 <?php get_header(); ?>
 			<p><?php include ("includes/logo.inc.php"); ?></p>
 			<?php include ("includes/navi-social.inc.php"); ?>
@@ -8,7 +12,7 @@
 		<div id="navi-cats" class="clear">
 			<div class="wrap">
 				<ul>
-					<li><a href="<?php echo  get_site_url(); ?>/latest-news/"><?php _e("Latest", 'storini'); ?></a></li>
+					<li><a href="<?php echo  get_site_url(); ?>/newyddion-diweddaraf/"><?php _e("Latest", 'storini'); ?></a></li>
 					<?php include ("includes/navi-cats.inc.php"); ?>
 				</ul>
 			</div>
@@ -32,7 +36,7 @@
 	<section id="article" class="clear">
 		<div class="wrap">
 
-			<div id="post-left-col">
+			<div id="post-left-col-no-ads">
 
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
@@ -57,7 +61,7 @@
 						<li><g:plusone size="medium" annotation="none"></g:plusone></li>
 						<li><a href="<?php echo  get_site_url(); ?>//pinterest.com/pin/create/button/" data-pin-do="buttonBookmark" ><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" alt="" /></a><span class="share-overlay"></span></li>
 						<li><script type="IN/Share" data-url="<?php the_permalink(); ?>" data-counter="right"></script></li>
-						<li><a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>">Tweet</a></li>
+						<li><a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-via="<?php bloginfo('name')?>">Tweet</a></li>
 					</ul>
 				</div>
 				<?php } else { ?>
@@ -65,34 +69,6 @@
 
 			</div>
 			<!--/post-left-col-->
-
-			<aside id="post-right-col">
-
-				<?php if( get_field('advertising', 'option') ) { ?>
-				<div class="clear article-mod mod-sponsor">
-					<h2 class="sub-header"><?php _e("Advertisements", 'storini'); ?></h2>
-					<div class="ads tile">
-						<?php include ("includes/ad-one.inc.php"); ?>
-						<?php if( get_field('ad_posts', 'option') ) { ?>
-						<?php include ("includes/ad-two.inc.php"); ?>
-						<?php } else { ?>
-						<?php } ?>
-					</div>
-				</div>
-				<?php } else { ?>
-				<?php } ?>
-
-				<div class="clear article-mod mod-related">
-					<h2 class="sub-header"><?php _e("Latest news", 'storini'); ?></h2>
-					<?php query_posts('showposts=1'); ?>
-					<?php while (have_posts()) : the_post(); ?>
-					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-					<p><?php the_time('j F Y'); ?></p>
-					<?php endwhile; ?>
-				</div>
-
-			</aside>
-			<!--/post-right-col-->
 
 		</div>
 	</section>
